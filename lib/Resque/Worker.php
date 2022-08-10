af354a76b4d0f417a0c7153d29317f2f8e1cd2fa
Phong Nguyen <phong.nguyen@trinet.com>
2022-08-10 15:55:33 +0700

    master: change method

--- lib/Resque/Worker.php
+++ lib/Resque/Worker.php
@@ -630,29 +630,27 @@ class Resque_Worker
             } else {
                 switch ($code) {
                     case self::LOG_TYPE_INFO:
-                        $this->logger->addInfo($message, $extra);
+                        $this->logger->info($message, $extra);
                         break;
                     case self::LOG_TYPE_WARNING:
-                        $this->logger->addWarning($message, $extra);
+                        $this->logger->warning($message, $extra);
                         break;
                     case self::LOG_TYPE_ERROR:
-                        $this->logger->addError($message, $extra);
+                        $this->logger->error($message, $extra);
                         break;
                     case self::LOG_TYPE_CRITICAL:
-                        $this->logger->addCritical($message, $extra);
+                        $this->logger->critical($message, $extra);
                         break;
                     case self::LOG_TYPE_ALERT:
-                        $this->logger->addAlert($message, $extra);
+                        $this->logger->alert($message, $extra);
                 }
             }
-
-
 
         } else if ($code === self::LOG_TYPE_DEBUG && $this->logLevel === self::LOG_VERBOSE) {
             if ($this->logger === null) {
                 fwrite($this->logOutput, "[" . date('c') . "] " . $message . "\n");
             } else {
-                $this->logger->addDebug($message, $extra);
+                $this->logger->debug($message, $extra);
             }
         } else {
             return false;
